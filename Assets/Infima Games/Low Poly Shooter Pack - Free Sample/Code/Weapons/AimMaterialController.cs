6266afd2e47d8b9a4ca2d7ccc8fdc337d1eaf485
2205Dima <mityanya2605@gmail.com>
2024-01-20 00:43:56 +0200

    Оптимізація прицілу

---
+++ Assets/Infima Games/Low Poly Shooter Pack - Free Sample/Code/Weapons/AimMaterialController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using Cinemachine;
+
+public class AimMaterialController : MonoBehaviour
+{
+    public Camera aimCamera;
+    public Renderer targetRenderer;
+    public Material aimMaterial;
+    public Material originalMaterial;
+
+    void Start()
+    {
+        if (targetRenderer == null)
+        {
+            Debug.LogError("Renderer не знайдено. Додайте Renderer до об'єкта або встановіть посилання в інспекторі.");
+            enabled = false;
+            return;
+        }
+
+        originalMaterial = targetRenderer.material;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            OnAimStop();
+        }
+        else
+        {
+            OnAim();
+        }
+    }
+
+    void OnAim()
+    {
+        // Вимкнути aimCamera при прицілюванні
+        aimCamera.enabled = false;
+
+        // Змінити матеріал при прицілюванні
+        targetRenderer.material = aimMaterial;
+    }
+
+    void OnAimStop()
+    {
+        // Вмикнути aimCamera при припиненні прицілювання
+        aimCamera.enabled = true;
+
+        // Повернути початковий матеріал при відпусканні кнопки
+        targetRenderer.material = originalMaterial;
+    }
+}
